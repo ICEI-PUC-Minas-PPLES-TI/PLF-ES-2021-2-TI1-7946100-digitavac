c3af88e9d3fa5e59a5bd572067b0fc3ef3779248
Matheus Andrade <90425126+MatheusMOAndrade@users.noreply.github.com>
2021-11-11 21:01:10 -0300

    Update cadastroHistorico.js

--- Codigo/js/cadastroHistorico.js
+++ Codigo/js/cadastroHistorico.js
@@ -1,15 +1,27 @@
+function verificaDados() {
+
+    const cadBtn = document.getElementById('cadastroBtn');
+    let newVacina = document.getElementById('vacField').value;
+    let newDataDaAplicacao = document.getElementById('aplicationField').value;
+    if (newVacina != '' && newDataDaAplicacao != '') {
+        cadBtn.removeAttribute('disabled');
+    }else{
+        cadBtn.setAttribute('disabled', 'true');
+    }
+
+}
+
 /*verifica se há dados inclusos no database do localstorage*/
 function dataRead() {
-    let strData = localStorage.getItem('db');
+    let strData = localStorage.getItem('dbVac');
     let objData = {};
 
-    if (strData) 
-    {
-        objData = JSON.parse (strData);  /* inclui a string com os dados lidos (strData) no objeto contendo os dados (objData) */
+    if (strData) {
+        objData = JSON.parse(strData);  /* inclui a string com os dados lidos (strData) no objeto contendo os dados (objData) */
     }
     else {
-        objData = { 
-            historico: []
+        objData = {
+            historico: [{ vacina: "", dataDaAplicacao: "", proximaAplicacao: "", observacoes: "" }]
         }
     }
 
@@ -20,34 +32,87 @@ function dataRead() {
 /* salva os dados no database do localstorage */
 function dataSave(data) {
 
-    localStorage.setItem ('db', JSON.stringify (data))
+    localStorage.setItem('dbVac', JSON.stringify(data))
 
 }
 
-/*adiciona uma nova vacina ao database do localstorage*/
-function dataAdd() {
-    /*leitura dos dados do localstorage*/
-    let objData = dataRead ();
 
-    /* incluir um novo contato */
+/* imprime os dados na página */
+function dataPrint(e) {
+
+    let screen = document.getElementById('screen');
+    let strHtml = '';
+    let objData = dataRead();
+
+    for (i = 0; i < objData.historico.length; i++) {
+        strHtml += `<p class="list-group-item list-group-item-action> <strong>Vacina: </strong>${objData.historico[i].vacina}; <strong>Data da aplicação: </strong>${objData.historico[i].dataDaAplicacao}; <strong>Data da aplicação (segunda dose): </strong>${objData.historico[i].proximaAplicacao}; <strong>Observações: </strong> ${objData.historico[i].observacoes}<\p>`
+    }
+
+    screen.innerHTML = strHtml;
+
+}
+
+
+/*adiciona uma nova vacina ao database do localstorage*/
+function dataAdd(e) {
+
+    const locale = 'pt-br'
+    /*leitura dos dados do localstorage*/
+    let objData = dataRead();
+
+    /* incluir as entradas em cada string */
     let newVacina = document.getElementById('vacField').value;
     let newDataDaAplicacao = document.getElementById('aplicationField').value;
     let newProximaAplicacao = document.getElementById('nextAplicationField').value;
     let newObservacoes = document.getElementById('observationField').value;
 
+
+    let arrayData = newDataDaAplicacao.split('-');
+    let aplicacaoDate = new Date(arrayData[0], arrayData[1] - 1, arrayData[2])
+
+    let concatenaDateAplic = arrayData[0] + '' + arrayData[1] + '' + arrayData[2];
+
+    arrayData = newProximaAplicacao.split('-');
+    let proximaAplicacaoDate = new Date(arrayData[0], arrayData[1] - 1, arrayData[2])
+
+    let concatenaDateProx = arrayData[0] + '' + arrayData[1] + '' + arrayData[2];
+
+    if ( parseInt(concatenaDateAplic) > parseInt(concatenaDateProx) && newProximaAplicacao != '') {
+        alert("A data da próxima dose deve ser maior que a primeira.");
+        return;
+    }
+
+    /* inclui as strings no objeto */
     let newObjVac = {
         vacina: newVacina,
-        dataDaAplicacao: newDataDaAplicacao,
-        proximaAplicacao: newProximaAplicacao,
+        dataDaAplicacao: aplicacaoDate.toLocaleDateString(locale).toString(),
+        proximaAplicacao: proximaAplicacaoDate.toLocaleDateString(locale).toString(),
         observacoes: newObservacoes
     };
 
-    objData.historico.push (newObjVac); /* adiciona o novo cadastro ao array histórico */
+    objData.historico.push(newObjVac); /* adiciona o novo cadastro ao array histórico */
 
     /* salvar no localstorage os novos dados */
     dataSave(objData);
-    
+
 }
 
-/* botão de cadastrar a vacina chama a função dataAdd*/
-document.getElementById ('registerBtn').addEventListener('click', dataAdd)
+function deleteData(e) {
+
+    objData = dataRead();
+
+    let deleteVac = document.getElementById('vacField').value;
+    let deleteDataDaAplicacao = document.getElementById('aplicationField').value;
+
+    for (let i = 0; i < objData.historico.length; i++) {
+
+        if (deleteVac == objData.historico[i].vacina && deleteDataDaAplicacao == objData.historico[i].dataDaAplicacao && deleteVac != '' && deleteDataDaAplicacao != '') {
+            objData.historico.splice(i, 1);
+            dataSave(objData);
+            alert("Vacina" + historico[i].vacina + "apagado com sucesso");
+            continue;
+        }
+    }
+
+}
+
